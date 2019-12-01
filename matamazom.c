a4bdbc2579061b3eb64718b5f1faf2cb2bf159e8
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2019-12-01 21:25:55 +0200

    start

--- matamazom.c
+++ matamazom.c
@@ -2,3 +2,19 @@
 #include "set.h"
 #include "amount_set.h"
 
+struct productInformation_t {
+  MtmProductData customData;
+  MatamazomAmountType amountType;
+  unsigned int id;
+  char *name;
+  unsigned int total_income;
+};
+
+int compareProductsID(unsigned int product_id1, unsigned int product_id2) {
+  return product_id1 - product_id2;
+}
+
+Matamazom matamazomCreate(ProductInfo product){
+
+}
+
