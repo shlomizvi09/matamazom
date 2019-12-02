d63decea3a3e6718a5e4f2ff4a56ee5d4ea2a689
Arik <berenshtein@campus.technion.ac.il>
2019-12-02 20:58:47 +0200

    amountVerification

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
+//#include "matamazom.h"
+#include <math.h>
+#include <stdio.h>
 #include "matamazom.h"
 
+#define HALF 0.5
+#define RANGE 0.001
+
+double amountVerifications(double amount, MatamazomAmountType type);
 
 int main() {
-  return 0;
+    double x = amountVerifications(0.00006, MATAMAZOM_HALF_INTEGER_AMOUNT);
+    printf("x is %f\n", x);
+    printf("ceil of x is %f\n", ceil(1.51));
+    double y = 1.51 - 1.5;
+    printf("%f", y);
+}
+
+double amountVerifications(double amount, MatamazomAmountType type) {
+    if (amount < 0) {
+        return -1;
+    }
+    if (type == MATAMAZOM_ANY_AMOUNT) {
+        return amount;
+    } else if (type == MATAMAZOM_INTEGER_AMOUNT) {
+        if ((fabs(amount - round(amount))) <= RANGE) {
+            return round(amount);
+        }
+        return -1;
+    } else if (type == MATAMAZOM_HALF_INTEGER_AMOUNT) {
+        if ((round(amount) - floor(amount)) == 0) {
+            if (fabs(amount - floor(amount) <= RANGE)) {
+                return floor(amount);
+            } else if (fabs(amount - floor(amount) - HALF <= RANGE)) {
+                return floor(amount) + HALF;
+            }
+            return -1;
+        } else if ((round(amount) - ceil(amount)) == 0) {
+            if (fabs(ceil(amount) - amount <= RANGE)) {
+                return ceil(amount);
+            } else if (fabs(ceil(amount - amount - HALF <= RANGE))) {
+                return (ceil(amount) - HALF);
+            }
+            return -1;
+        }
+    }
+    return -1;
 }
--- matamazom.c
+++ matamazom.c
@@ -3,6 +3,7 @@
 #include "amount_set.h"
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 struct productInformation_t {
   MtmProductData customData;
@@ -15,6 +16,7 @@ struct productInformation_t {
   unsigned int total_income;
 };
 
+double amountVerifications(double amount, MatamazomAmountType type);
 struct Matamazom_t {
   AmountSet products;
   AmountSet orders;
@@ -69,4 +71,35 @@ Matamazom matamazomCreate() {
     free(new_warehouse);
     return NULL;
   }
+}
+
+double amountVerifications(double amount, MatamazomAmountType type) {
+    if (amount < 0) {
+        return -1;
+    }
+    if (type == MATAMAZOM_ANY_AMOUNT) {
+        return amount;
+    } else if (type == MATAMAZOM_INTEGER_AMOUNT) {
+        if ((fabs(amount - round(amount))) <= RANGE) {
+            return round(amount);
+        }
+        return -1;
+    } else if (type == MATAMAZOM_HALF_INTEGER_AMOUNT) {
+        if ((round(amount) - floor(amount)) == 0) {
+            if (fabs(amount - floor(amount) <= RANGE)) {
+                return floor(amount);
+            } else if (fabs(amount - floor(amount) - HALF <= RANGE)) {
+                return floor(amount) + HALF;
+            }
+            return -1;
+        } else if ((round(amount) - ceil(amount)) == 0) {
+            if (fabs(ceil(amount) - amount <= RANGE)) {
+                return ceil(amount);
+            } else if (fabs(ceil(amount - amount - HALF <= RANGE))) {
+                return (ceil(amount) - HALF);
+            }
+            return -1;
+        }
+    }
+    return -1;
 }
