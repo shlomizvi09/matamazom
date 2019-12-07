4e4b0b19fcaa1a0497ca9786674438e3a149cab1
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2019-12-07 19:36:12 +0200

    FINAL CODE with comments

--- amount_set.c
+++ amount_set.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define ERROR -1
+
 typedef struct node_t {
   ASElement element;
   double amount;
   struct node_t *next;
 } *Node;
 struct AmountSet_t {
-  CopyASElement as_copy;
-  FreeASElement as_free;
-  CompareASElements as_compare;
-  Node head;
+  CopyASElement user_copy_function;
+  FreeASElement user_free_function;
+  CompareASElements user_compare_function;
+  Node head; // the start of a linked list. 'head' is a dummy.
   Node iterator;
 };
 static Node getElementNodePtr(AmountSet set, ASElement element);
@@ -26,14 +28,16 @@ AmountSet asCreate(CopyASElement copyElement,
   if (new_set == NULL) {
     return NULL;
   }
-  new_set->as_compare = compareElements;
-  new_set->as_free = freeElement;
-  new_set->as_copy = copyElement;
+  // initializing all fields
+  new_set->user_compare_function = compareElements;
+  new_set->user_free_function = freeElement;
+  new_set->user_copy_function = copyElement;
   new_set->head = malloc(sizeof(*(new_set->head)));
   if (new_set->head == NULL) {
     free(new_set);
     return NULL;
   }
+  // first node in linked list is a dummy
   new_set->head->next = NULL;
   new_set->head->element = NULL;
   new_set->head->amount = 0;
@@ -44,16 +48,20 @@ void asDestroy(AmountSet set) {
   if (set == NULL) {
     return;
   }
-  Node temp_ptr = NULL;
-  Node temp_ptr2 = set->head->next;
-  while (temp_ptr2 != NULL) {
-    assert(temp_ptr2 != set->head);
-
-    set->as_free(temp_ptr2->element);
-    temp_ptr = temp_ptr2;
-    temp_ptr2 = temp_ptr2->next;
-    free(temp_ptr);
+  /* going through every the list's node, making sure it isn't NULL,
+   * using the user's free function, freeing the element and then the node. */
+  Node prior_node = NULL;
+  Node current_node = set->head->next;
+  while (current_node != NULL) {
+    assert(current_node != set->head);
+    set->user_free_function(current_node->element);
+    prior_node = current_node;
+    current_node = current_node->next;
+    free(prior_node);
   }
+  /* eventually, freeing the dummy node and the set itself.
+   * the internal iterator may point somewhere, but all the nodes are
+   * already freed so no need to free the iterator as well.*/
   free(set->head);
   free(set);
 }
@@ -63,12 +71,15 @@ bool asContains(AmountSet set, ASElement element) {
     return false;
   }
   Node node_ptr;
+  /* going through all the nodes in the list until we find the element, using
+   * the user's compare function, or until the list is over. */
   for (node_ptr = set->head->next; node_ptr != NULL;
        node_ptr = node_ptr->next) {
     if (node_ptr->element == NULL) {
+      // there shouldn't be a NULL element in the middle of the list .
       return false;
     }
-    if (set->as_compare(element, node_ptr->element) == 0) {
+    if (set->user_compare_function(element, node_ptr->element) == 0) {
       return true;
     }
   }
@@ -77,9 +88,11 @@ bool asContains(AmountSet set, ASElement element) {
 
 int asGetSize(AmountSet set) {
   if (set == NULL) {
-    return -1;
+    return ERROR; //error value
   }
   int size = 0;
+  /* going through all the linked list, promoting the size value until list
+   is over */
   Node node_ptr = set->head->next;
   while (node_ptr != NULL) {
     node_ptr = node_ptr->next;
@@ -97,6 +110,7 @@ AmountSetResult asGetAmount(AmountSet set, ASElement element, double
   if (asContains(set, element) == false) {
     return AS_ITEM_DOES_NOT_EXIST;
   }
+  // extracting the element's amount
   node_ptr = getElementNodePtr(set, element);
   *outAmount = node_ptr->amount;
   return AS_SUCCESS;
@@ -106,34 +120,44 @@ AmountSet asCopy(AmountSet set) {
   if (set == NULL) {
     return NULL;
   }
-  AmountSet new_set = asCreate(set->as_copy, set->as_free, set->as_compare);
+  // creating a new empty AS with the given set's functions.
+  AmountSet new_set = asCreate(set->user_copy_function,
+                               set->user_free_function,
+                               set->user_compare_function);
   if (new_set == NULL) {
     return NULL;
   }
   Node node_ptr_copy_from = set->head->next;
+  /* a pointer the the first node in AS's linked list.
+   * will be promoted later */
   if (node_ptr_copy_from == NULL) {
+    // if AS is empty, return an empty one as well.
     return new_set;
   }
   while (node_ptr_copy_from != NULL) {
     AmountSetResult result = asRegister(new_set,
                                         node_ptr_copy_from->element);
+    // adding the element from node_ptr_copy_from to the new AS.
     if (result == AS_NULL_ARGUMENT) {
+      // if failed, we must free the allocated memory.
       asDestroy(new_set);
       return NULL;
     }
+    // we need a pointer to the nodes who holds the element
     Node node_ptr_copy_to =
         getElementNodePtr(new_set,
                           node_ptr_copy_from->element);
 
     assert(node_ptr_copy_to != NULL && node_ptr_copy_to->next == NULL);
-    // because the set is new and empty
+
+    // assigning all fields.
     node_ptr_copy_to->amount = node_ptr_copy_from->amount;
     node_ptr_copy_from = node_ptr_copy_from->next;
   }
   Node ptr_to_first_element =
       getElementNodePtr(new_set, set->head->next->element);
+  // we need a pointer to first node to connect the new set to the linked list.
   new_set->head->next = ptr_to_first_element;
-  // connects the new set to the linked list
   return new_set;
 }
 
@@ -146,20 +170,24 @@ AmountSetResult asRegister(AmountSet set, ASElement element) {
   }
   Node node_before = set->head;
   Node node_after = set->head->next;
+  // will be used to connect the linked list properly.
+
   while (node_after != NULL) {
-    if (set->as_compare(element, node_after->element) < 0) {
+    if (set->user_compare_function(element, node_after->element) < 0) {
       break;
-      /*loop runs until it reaches a bigger element (by as_compare)
+      /*loop runs until it reaches a bigger element (by user_compare_function)
        * or the end of the list (NULL) */
     }
     node_before = node_after;
     node_after = node_after->next;
   }
   Node new_node = malloc(sizeof(*new_node));
+  // the node which will hold the element.
   if (new_node == NULL) {
     return AS_OUT_OF_MEMORY;
   }
-  new_node->element = set->as_copy(element);
+  // assigning all field.
+  new_node->element = set->user_copy_function(element);
   new_node->amount = 0;
   new_node->next = node_after != NULL ? node_after : NULL;
   node_before->next = new_node;
@@ -175,17 +203,22 @@ AmountSetResult asDelete(AmountSet set, ASElement element) {
   }
   Node node_before = set->head;
   Node node_to_delete = set->head->next;
+  // will be used to connect the linked list properly.
+
+  // the loop rubs until the element is found or the linked list is over
   while (node_to_delete != NULL) {
-    if (set->as_compare(element, node_to_delete->element) == 0) {
+    if (set->user_compare_function(element, node_to_delete->element) == 0) {
       break;
     }
     node_before = node_to_delete;
     node_to_delete = node_to_delete->next;
   }
   if (node_to_delete == NULL) {
+    // if linked list is over and the element isn't found.
     return AS_ITEM_DOES_NOT_EXIST;
   }
-  set->as_free(node_to_delete->element);
+  // freeing the element and connecting the nodes properly.
+  set->user_free_function(node_to_delete->element);
   node_before->next = node_to_delete->next;
   free(node_to_delete);
   return AS_SUCCESS;
@@ -195,12 +228,15 @@ AmountSetResult asClear(AmountSet set) {
   if (set == NULL) {
     return AS_NULL_ARGUMENT;
   }
-  Node node_ptr = set->head->next;
-  while (node_ptr != NULL) {
-    Node curr_node = node_ptr;
-    set->as_free(curr_node->element);
-    node_ptr = node_ptr->next;
-    free(curr_node);
+  // a pointer to the first node in linked list, will be promoted later.
+  Node next_node = set->head->next;
+  Node prior_node = NULL;
+  while (next_node != NULL) {
+    // as long as the linked list isn't over
+    prior_node = next_node;
+    set->user_free_function(prior_node->element);
+    next_node = next_node->next;
+    free(prior_node);
   }
   set->head->next = NULL;
   return AS_SUCCESS;
@@ -214,6 +250,7 @@ amount) {
   if (asContains(set, element) == false) {
     return AS_ITEM_DOES_NOT_EXIST;
   }
+  // getting the node that holds the element
   Node node_of_element = getElementNodePtr(set, element);
   assert(node_of_element != NULL);
   if (node_of_element->amount + amount < 0) {
@@ -225,14 +262,17 @@ amount) {
 
 ASElement asGetFirst(AmountSet set) {
   if (set == NULL || set->head->next == NULL) {
+    //if the list is empty
     return NULL;
   }
+  // the head is dummy. elements start at head->next .
   set->iterator = set->head->next;
   return set->iterator->element;
 }
 
 ASElement asGetNext(AmountSet set) {
   if (set == NULL || set->head->next == NULL) {
+    // if the list is empty
     return NULL;
   }
   set->iterator = set->iterator->next;
@@ -242,13 +282,18 @@ ASElement asGetNext(AmountSet set) {
   return set->iterator->element;
 }
 
+
+/* the function receives the AS and a wanted element,
+ * and going through the linked list until element is found
+ * (if exists) and returning a pointer to the node that holds
+ * the wanted element. */
 static Node getElementNodePtr(AmountSet set, ASElement element) {
   if (set == NULL || element == NULL) {
     return NULL;
   }
   Node node_ptr = set->head->next;
   while (node_ptr != NULL) {
-    if (set->as_compare(node_ptr->element, element) == 0) {
+    if (set->user_compare_function(node_ptr->element, element) == 0) {
       return node_ptr;
     }
     node_ptr = node_ptr->next;
--- matamazom.c
+++ matamazom.c
@@ -30,12 +30,17 @@ struct Matamazom_t {
   AmountSet products;
   List orders;
   unsigned int max_order_id;
+  /* in case of removing an order from the list, max_order_id making sure that
+   * indexes are always getting bigger to avoid repeating.*/
 };
 
 static bool isOrderExists(Matamazom matamazom, const unsigned int orderId) {
   if (matamazom == NULL) {
     return false;
   }
+  /* going through all orders, returning true if we find the relevent ordr,
+   according to orderId.
+   return false otherwise. */
   LIST_FOREACH(Order, iteraor, matamazom->orders) {
     if (iteraor->order_id == orderId) {
       return true;
@@ -48,9 +53,12 @@ static Order getOrder(Matamazom matamazom, const unsigned int orderId) {
   if (matamazom == NULL) {
     return NULL;
   }
-  LIST_FOREACH(Order, iteraor, matamazom->orders) {
-    if (iteraor->order_id == orderId) {
-      return iteraor;
+  /*going through all orders, returning a pointer to the Order struct
+   * if we find the relevant order, according to orderId.
+   *return NULL otherwise*/
+  LIST_FOREACH(Order, iterator, matamazom->orders) {
+    if (iterator->order_id == orderId) {
+      return iterator;
     }
   }
   return NULL;
@@ -58,7 +66,16 @@ static Order getOrder(Matamazom matamazom, const unsigned int orderId) {
 
 static bool isAmountValid(double amount_to_change, MatamazomAmountType
 type) {
+  /* making sure that the amount given by the user is valid,
+  which means it correlates with the amount type,
+   and it's withing a range of RANGE=0.001 withing the closer correct number.*/
   double amount = fabs(amount_to_change);
+  /* there are 4 case: 1. the number is anyAmount type.
+   * 2. the number is integer type, to is must be 0.001 or less close to the
+   * closest integer.
+   * 3. half-integer type, which means it has to be withing the range of 0.001
+   * to floor(amount) (closest lower integer of amount) or ceil(amount)
+   * (closest higher integer of amount) */
   if (type == MATAMAZOM_ANY_AMOUNT) {
     return true;
   } else if (type == MATAMAZOM_INTEGER_AMOUNT) {
@@ -87,12 +104,14 @@ type) {
 }
 
 static ProductInfo findProductInfo(AmountSet set, unsigned int id) {
+  // going through all the products and return a pointer to a given order.
   ProductInfo iterator = (ProductInfo) asGetFirst(set);
   if (iterator == NULL) {
     return NULL;
   }
   while (iterator != NULL) {
     if (iterator->id == id) {
+      // product is found
       return iterator;
     }
     iterator = (ProductInfo) asGetNext(set);
@@ -103,25 +122,30 @@ static ProductInfo findProductInfo(AmountSet set, unsigned int id) {
 
 static MatamazomAmountType getAmountType(const unsigned int productId, Matamazom
 matamazom) {
-  ProductInfo info = findProductInfo(matamazom->products, productId);
-  return info->amountType;
+  ProductInfo product_info = findProductInfo(matamazom->products, productId);
+  // return the wanted product's amount type
+  return product_info->amountType;
 }
 
 static bool isNameValid(const char *name) {
+  // making sure the name is valid
   return ((*name >= 'a' && *name <= 'z') || (*name >= 'A' && *name <= 'Z')
       || (*name >= '0' && *name <= '9'));
 }
 
 int compareProductsID(ASElement product_id1, ASElement product_id2) {
+  // a function we provied the AmountSet as users.
   return (int) (((ProductInfo) product_id1)->id
       - ((ProductInfo) product_id2)->id);
 }
 
 int compareOrdersID(Order order_1, Order order_2) {
+  // a function we provied the List as users.
   return ((int) (order_1->order_id) - (int) (order_2->order_id));
 }
 
 void freeProduct(ASElement element) {
+  // a function we provied the AmountSet as users.
   if (element == NULL) {
     return;
   }
@@ -132,6 +156,7 @@ void freeProduct(ASElement element) {
 }
 
 void freeOrders(ListElement element) {
+  // a function we provied the List as users.
   if (element == NULL) {
     return;
   }
@@ -141,6 +166,7 @@ void freeOrders(ListElement element) {
 }
 
 ASElement copyProductInfo(ASElement element) {
+  // a function we provied the AmountSet as users.
   if (element == NULL) {
     return NULL;
   }
@@ -169,6 +195,7 @@ ASElement copyProductInfo(ASElement element) {
 }
 
 ListElement copyOrder(ListElement element) {
+  // a function we provied the List as users.
   if (element == NULL) {
     return NULL;
   }
@@ -187,6 +214,7 @@ ListElement copyOrder(ListElement element) {
 }
 
 Matamazom matamazomCreate() {
+  // creating the AS for product and List for orders
   Matamazom new_warehouse = malloc(sizeof(*new_warehouse));
   if (new_warehouse == NULL) {
     return NULL;
@@ -204,10 +232,11 @@ Matamazom matamazomCreate() {
     free(new_warehouse);
     return NULL;
   }
+  // initializing max order is, since there are no orders yet.
   new_warehouse->max_order_id = 0;
   return new_warehouse;
 }
-
+// destroying the product (AS) and the orders (List)
 void matamazomDestroy(Matamazom matamazom) {
   if (matamazom == NULL) {
     return;
@@ -230,6 +259,9 @@ MatamazomResult mtmNewProduct(Matamazom matamazom,
                               MtmCopyData copyData,
                               MtmFreeData freeData,
                               MtmGetProductPrice prodPrice) {
+  /* ** if allocation fails at any level, we must free all the memory allocated
+  so far! **  */
+
   if (matamazom == NULL || name == NULL || customData == NULL
       || copyData == NULL || freeData == NULL || prodPrice == NULL) {
     return MATAMAZOM_NULL_ARGUMENT;
@@ -244,9 +276,11 @@ MatamazomResult mtmNewProduct(Matamazom matamazom,
     return MATAMAZOM_INVALID_AMOUNT;
   }
   ProductInfo new_product = malloc(sizeof(*new_product));
+  //allocating a place for a new product_info (ASElement in out case)
   if (new_product == NULL) {
     return MATAMAZOM_OUT_OF_MEMORY;
   }
+  // initializing all fields
   new_product->id = id;
   new_product->copyData = copyData;
   new_product->freeData = freeData;
@@ -254,11 +288,13 @@ MatamazomResult mtmNewProduct(Matamazom matamazom,
   new_product->amountType = amountType;
   new_product->total_income = 0;
   new_product->customData = new_product->copyData(customData);
+  //using the user's copy function, since we need a copy of the customData
   if (new_product->customData == NULL) {
     free(new_product);
     return MATAMAZOM_OUT_OF_MEMORY;
   }
   new_product->name = malloc(strlen(name) + 1);
+  // allocating memory for the name
   if (new_product->name == NULL) {
     freeProduct(new_product);
     return MATAMAZOM_OUT_OF_MEMORY;
@@ -266,12 +302,15 @@ MatamazomResult mtmNewProduct(Matamazom matamazom,
   new_product->name = strcpy(new_product->name, name);
 
   if (asContains(matamazom->products, new_product)) {
+    /* if the product already exist, we must undo what we did so far.
+     * we created the product_info so we could check if it existed. */
     new_product->freeData(new_product->customData);
     free(new_product->name);
     free(new_product);
     return MATAMAZOM_PRODUCT_ALREADY_EXIST;
   }
   AmountSetResult result = asRegister(matamazom->products, new_product);
+  // sending a copy of it to the AS
   if (result == AS_NULL_ARGUMENT) {
     freeProduct(new_product); // asRegister uses a copy of product
     return MATAMAZOM_NULL_ARGUMENT;
@@ -292,14 +331,17 @@ MatamazomResult mtmChangeProductAmount(Matamazom matamazom,
   if (matamazom == NULL) {
     return MATAMAZOM_NULL_ARGUMENT;
   }
+  //finding the procuct_info's pointer
   ProductInfo product_info = findProductInfo(matamazom->products, id);
   if (product_info == NULL) {
     return MATAMAZOM_PRODUCT_NOT_EXIST;
   }
+  //making sure the added amount is legal.
   if (!isAmountValid(amount, product_info->amountType)) {
     return MATAMAZOM_INVALID_AMOUNT;
   }
   AmountSetResult
+  // changing the amount in the AS
       result = asChangeAmount(matamazom->products, product_info,
                               amount);
   if (result == AS_NULL_ARGUMENT) {
@@ -319,15 +361,18 @@ MatamazomResult mtmClearProduct(Matamazom matamazom, const unsigned int id) {
   if (matamazom == NULL) {
     return MATAMAZOM_NULL_ARGUMENT;
   }
-  ProductInfo id_ptr = findProductInfo(matamazom->products, id);
-  if (id_ptr == NULL) {
+  //finding the product_info's pointer
+  ProductInfo product_info_ptr = findProductInfo(matamazom->products, id);
+  if (product_info_ptr == NULL) {
     return MATAMAZOM_PRODUCT_NOT_EXIST;
   }
-  asDelete(matamazom->products, (ASElement) id_ptr);
+  //deleting the product from products (AS)
+  asDelete(matamazom->products, (ASElement) product_info_ptr);
   LIST_FOREACH(Order, element, matamazom->orders) {
-    id_ptr = findProductInfo(element->cart, id);
-    if (id_ptr != NULL) {
-      asDelete(element->cart, (ASElement) id_ptr);
+    //going through every order and if the product is in it, it will be removed
+    product_info_ptr = findProductInfo(element->cart, id);
+    if (product_info_ptr != NULL) {
+      asDelete(element->cart, (ASElement) product_info_ptr);
     }
 
   }
@@ -339,11 +384,16 @@ unsigned int mtmCreateNewOrder(Matamazom matamazom) {
     return 0;
   }
   unsigned int max_id = matamazom->max_order_id;
+  /*making sure we won't initialize an order is that already deleted from
+  the List */
   Order current_order = (Order) malloc(sizeof(*current_order));
+  // allocating memory for an order struct.
   if (current_order == NULL) {
     return 0;
   }
+  // assigning field.
   current_order->order_id = max_id + 1;
+  //creating a shopping cart AS
   current_order->cart =
       asCreate(copyProductInfo, freeProduct, compareProductsID);
   if (current_order->cart == NULL) {
@@ -352,12 +402,15 @@ unsigned int mtmCreateNewOrder(Matamazom matamazom) {
   }
   ListResult
       result = listInsertLast(matamazom->orders, (ListElement) current_order);
+  /* listInsertLast inserts a *copy* of the order, now we need to release the source,
+  no matter what is the value of result */
   asDestroy(current_order->cart);
   free(current_order);
   if (result == LIST_OUT_OF_MEMORY || result == LIST_NULL_ARGUMENT) {
     return 0;
   }
   matamazom->max_order_id = max_id + 1;
+  //promoting the max_order_id field.
   return max_id + 1;
 }
 
@@ -369,12 +422,17 @@ MatamazomResult mtmShipOrder(Matamazom matamazom, const unsigned int orderId) {
   if (!isOrderExists(matamazom, orderId)) {
     return MATAMAZOM_ORDER_NOT_EXIST;
   }
+  // fetching the order struct's pointer
   Order order = getOrder(matamazom, orderId);
+  /* NULL cases already checked, and order is in the list,
+   * so 'order' shouldn't be NULL*/
   assert(order != NULL);
   ProductInfo current_product_in_order = asGetFirst(order->cart);
   double amount_in_order = 0;
   double amount_in_warehouse = 0;
   AmountSetResult result;
+  /*going through all the products in the cart, checking the amount in the
+   * products AS is sufficient */
   while (current_product_in_order != NULL) {
     result =
         asGetAmount(order->cart, current_product_in_order,
@@ -394,9 +452,13 @@ MatamazomResult mtmShipOrder(Matamazom matamazom, const unsigned int orderId) {
     }
     current_product_in_order = asGetNext(order->cart);
   }
+  /*now we know the amount of every product is sufficient, so we can start
+  shipping the order */
   double product_price_in_order = 0;
   current_product_in_order = asGetFirst(order->cart);
   ProductInfo current_product_in_products = NULL;
+  /*every product is removed from the products, by the amount in the order,
+   * and his income is updated in product_info */
   while (current_product_in_order != NULL) {
     current_product_in_products =
         findProductInfo(matamazom->products,
@@ -487,23 +549,30 @@ mtmChangeProductAmountInOrder(Matamazom matamazom, const unsigned int orderId,
     return MATAMAZOM_INVALID_AMOUNT;
   }
   if (amount == 0) {
+    // as said in the comments in matamazom.h, nothing should be done
     return MATAMAZOM_SUCCESS;
   }
   double outamount = 0;
 
+  //fetching the order's pointer in the list
   Order order_ptr = getOrder(matamazom, orderId);
   ProductInfo product_info = findProductInfo(matamazom->products, productId);
   asGetAmount(order_ptr->cart, product_info, &outamount);
+  // now 'outamount' holds the product's amount in the order
   double amount_after_change = outamount + amount;
+  //checking the amount is valid
   if (amount_after_change > 0) {
     if (asContains(order_ptr->cart, product_info)) {
       asChangeAmount(order_ptr->cart, product_info, amount);
       return MATAMAZOM_SUCCESS;
     }
+    // in case the orders doesn't exist, we create it
     asRegister(order_ptr->cart, (ASElement) product_info);
     asChangeAmount(order_ptr->cart, product_info, amount);
     return MATAMAZOM_SUCCESS;
   }
+  /* we will get here if the amount_after_change isn't positive,
+   * which means we need to remove it. */
   asDelete(order_ptr->cart, product_info);
   return MATAMAZOM_SUCCESS;
 
@@ -517,7 +586,10 @@ mtmPrintOrder(Matamazom matamazom, const unsigned int orderId, FILE *output) {
   if (isOrderExists(matamazom, orderId) == false) {
     return MATAMAZOM_ORDER_NOT_EXIST;
   }
+  //fetching the order struct's pointer
   Order order_ptr = getOrder(matamazom, orderId);
+
+  // next variables will be used to print the data according to instructions
   double total_price = 0;
   double price_of_each = 0;
   double amount_of_each = 0;
@@ -546,14 +618,17 @@ MatamazomResult mtmPrintBestSelling(Matamazom matamazom, FILE *output) {
   }
   while (product != NULL) {
     if (product->total_income == best_selling_product->total_income) {
+      // if incomes are equal, the one with the lower is will be selected
       if (product->id < best_selling_product->id) {
         best_selling_product = product;
         max_income = best_selling_product->total_income;
       }
     } else if (product->total_income > best_selling_product->total_income) {
+      //promoting the best selling through the entire list
       best_selling_product = product;
       max_income = best_selling_product->total_income;
     }
+    // promotion
     product = asGetNext(matamazom->products);
   }
   if (max_income == 0) {
@@ -576,6 +651,7 @@ mtmPrintFiltered(Matamazom matamazom, MtmFilterProduct customFilter,
   }
   double price_of_each = 0;
   double amount_of_each = 0;
+  //printing according to customFilter function by the user
   AS_FOREACH(ProductInfo, iterator, matamazom->products) {
     asGetAmount(matamazom->products, iterator, &amount_of_each);
     price_of_each = iterator->prodPrice(iterator->customData, 1);
